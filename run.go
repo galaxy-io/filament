d1f4cf8b20f21072a9c5e77935ba2830667d86f5
atterpac <michael@getgalaxy.io>
2026-07-10 11:30:09 -0600

    fix snapshot upsert reporting case

--- run.go
+++ run.go
@@ -296,6 +296,12 @@ func SourcePolicyForIngestion(t IngestionType) SourcePolicy {
 			Ordered:       true,
 			Checkpointing: CheckpointAfterCommit,
 		}
+	case IngestionSnapshotUpsert:
+		return SourcePolicy{
+			Mode:          ModeFull,
+			EmitsOps:      []Operation{OpInsert},
+			Checkpointing: CheckpointAfterBatch,
+		}
 	case IngestionUpsert, IngestionDelete:
 		return SourcePolicy{
 			Mode:          ModeIncremental,
