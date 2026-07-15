4e02b3921460bd7ba5c8c3f78c0726b567a26bef
atterpac <michael@getgalaxy.io>
2026-07-15 10:39:23 -0400

    remove duplicated secrets interface
    infra.go holds the main

--- secret/errors.go
+++
@@ -1,9 +0,0 @@
-package secret
-
-import "errors"
-
-// ErrNotFound is returned when a referenced secret does not exist.
-var ErrNotFound = errors.New("secret not found")
-
-// ErrReadOnly is returned by providers that do not support writes.
-var ErrReadOnly = errors.New("secret provider is read-only")
