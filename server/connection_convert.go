8dc64e8d40aae17216d7df9ff72268d7e62f4756
atterpac <michael@getgalaxy.io>
2026-07-21 09:07:12 -0600

    proto conversions

--- server/connection_convert.go
+++ server/connection_convert.go
@@ -11,12 +11,12 @@ func connectionFromProto(c *ingestionv1.Connection) filament.Connection {
 	if c == nil {
 		return filament.Connection{}
 	}
-	return filament.Connection{ID: c.GetId(), Tenant: c.GetTenant(), Kind: connectionKindFromProto(c.GetKind()), Name: c.GetName(), Connector: c.GetConnector(), Config: structMap(c.GetConfig()), SecretRefs: cloneStrings(c.GetSecretRefs()), Version: c.GetVersion()}
+	return filament.Connection{ID: c.GetId(), Tenant: c.GetTenantId(), Kind: connectionKindFromProto(c.GetKind()), Name: c.GetName(), Connector: c.GetConnector(), Config: structMap(c.GetConfig()), SecretRefs: cloneStrings(c.GetSecretRefs()), Version: c.GetVersion()}
 }
 
 func connectionToProto(c filament.Connection) *ingestionv1.Connection {
 	cfg, _ := structpb.NewStruct(c.Config)
-	return &ingestionv1.Connection{Id: c.ID, Tenant: c.Tenant, Kind: connectionKindToProto(c.Kind), Name: c.Name, Connector: c.Connector, Config: cfg, SecretRefs: cloneStrings(c.SecretRefs), Version: c.Version}
+	return &ingestionv1.Connection{Id: c.ID, TenantId: c.Tenant, Kind: connectionKindToProto(c.Kind), Name: c.Name, Connector: c.Connector, Config: cfg, SecretRefs: cloneStrings(c.SecretRefs), Version: c.Version}
 }
 
 func connectionKindFromProto(k ingestionv1.ConnectorKind) filament.ConnectorKind {
