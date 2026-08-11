2095c71aafd5d27e672e16ef7e92ffe9604eaa32
Mitchell Bregman <mitchbregs@gmail.com>
2026-08-11 13:44:43 -0400

    lint and format

--- ui/src/pages/pipelines/components/table/PipelinesTable.tsx
+++ ui/src/pages/pipelines/components/table/PipelinesTable.tsx
@@ -77,9 +77,7 @@ const PIPELINES_TABLE_COLUMNS: ColumnDef<Pipeline>[] = [
     cellLoading: () => <TextShimmer width={64} height={14} />,
     cell: ({ row }) => (
       <Text size={TextSize.BODY_SM} isEllipsis>
-        {row.original.lastRunAt > 0n
-          ? formatTimeAgo(row.original.lastRunAt)
-          : "—"}
+        {row.original.lastRunAt > 0n ? formatTimeAgo(row.original.lastRunAt) : "—"}
       </Text>
     ),
   },
@@ -109,9 +107,7 @@ const PIPELINES_TABLE_COLUMNS: ColumnDef<Pipeline>[] = [
     cellLoading: () => <TextShimmer width={52} height={14} />,
     cell: ({ row }) => (
       <Text size={TextSize.BODY_SM} isMonospace>
-        {row.original.lastRunAt > 0n
-          ? formatBytes(row.original.lastRunBytes)
-          : "—"}
+        {row.original.lastRunAt > 0n ? formatBytes(row.original.lastRunBytes) : "—"}
       </Text>
     ),
   },
@@ -147,12 +143,7 @@ const PipelinesTable = ({
         getRowId={(pipeline) => pipeline.id}
         contentWhenEmpty={
           <EmptyLayout
-            icon={
-              <Icon
-                component={MagnifyingGlassIcon}
-                variant={IconVariant.TERTIARY}
-              />
-            }
+            icon={<Icon component={MagnifyingGlassIcon} variant={IconVariant.TERTIARY} />}
             message="No pipelines match your search"
           />
         }
