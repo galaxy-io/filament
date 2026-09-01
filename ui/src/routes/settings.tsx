a142d9efb4e8d92d7c0b9d1a1fdf25897f4172b0
Mitchell Bregman <mitchbregs@gmail.com>
2026-09-01 14:02:18 -0400

    edits settings and auth

--- ui/src/routes/settings.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import SettingsPage from "@/pages/settings/SettingsPage";
-
-export const Route = createFileRoute("/settings")({
-  component: SettingsPage,
-});
