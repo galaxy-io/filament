7536069ba69cef57138cdf734acc4d4d54c6a08b
atterpac <michael@getgalaxy.io>
2026-09-16 08:50:24 -0600

    sqlite querires

---
+++ datastore/sqlite/queries/stream_epochs.sql
@@ -0,0 +1,9 @@
+-- name: GetStreamEpoch :one
+SELECT * FROM stream_epochs WHERE stream_id = sqlc.arg(stream_id) AND tenant_id = sqlc.arg(tenant_id) AND epoch = sqlc.arg(epoch);
+
+-- name: CreateStreamEpoch :one
+INSERT INTO stream_epochs(stream_id,tenant_id,epoch,attempt_token,certificate,positions)
+VALUES(sqlc.arg(stream_id),sqlc.arg(tenant_id),sqlc.arg(epoch),sqlc.arg(attempt_token),sqlc.arg(certificate),sqlc.arg(positions)) RETURNING *;
+
+-- name: AdvanceStreamEpoch :execrows
+UPDATE replication_streams SET last_epoch = sqlc.arg(epoch) WHERE id = sqlc.arg(stream_id) AND tenant_id = sqlc.arg(tenant_id) AND last_epoch = sqlc.arg(previous_epoch);
