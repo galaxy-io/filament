53e1fe3f4b49b9b0d792080dacad8bdeeb1c4a7c
leonkozlowski <leonkozlowski@gmail.com>
2026-09-24 13:38:47 -0400

    fix(connectors/http): inline comments

--- source.go
+++ source.go
@@ -251,7 +251,7 @@ type ConnectorSpec struct {
 	DarkLogoURL    string
 	LightLogoURL   string
 	Version        string
-	APIVersion     string // upstream API version; empty for unversioned APIs and drivers
+	APIVersion     string
 	Maturity       ConnectorMaturity
 	Modes          []ReadMode
 	SourcePolicies []SourcePolicy
